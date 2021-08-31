87ce53bba3ab059cd647666b77fe64b9043c0b37
whoisacat <whoisacat@gmail.com>
2021-08-31 15:39:23 +0500

    API-и недоступны, собрал как вроде должно работать, тянуть дальше смысла нет.

--- app/src/main/java/otus/homework/coroutines/MainActivity.kt
+++ app/src/main/java/otus/homework/coroutines/MainActivity.kt
@@ -2,29 +2,52 @@ package otus.homework.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import java.net.SocketTimeoutException
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var catsPresenter: CatsPresenter
-
     private val diContainer = DiContainer()
+
+    private val viewModel: CatsViewModel by viewModels { ViewModelFactory(diContainer.catsService, diContainer.imagesService) }
+
+    private lateinit var catsView: CatsView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
+        catsView = view
         setContentView(view)
 
-        catsPresenter = CatsPresenter(diContainer.service)
-        view.presenter = catsPresenter
-        catsPresenter.attachView(view)
-        catsPresenter.onInitComplete()
+        view.viewModel = viewModel
+        viewModel.onInitComplete()
+
+        viewModel.getFactState.observe(this, catsFactObserver)
     }
 
-    override fun onStop() {
-        if (isFinishing) {
-            catsPresenter.detachView()
+    private val catsFactObserver = Observer<Result?>{
+        when(it) {
+            Empty -> {}
+            is Error -> {
+                when(it.t) {
+                    is SocketTimeoutException -> showToast("Не удалось получить ответ от сервера")
+                    else -> showToast(it.t?.message.toString())
+                }
+                viewModel.onMessageShown()
+            }
+            is Success -> {
+                catsView.populate(it.data)
+            }
         }
-        super.onStop()
     }
+
+    private fun showToast(msg: String) =
+        Toast.makeText(
+            this, msg,
+            Toast.LENGTH_LONG
+        ).show()
+
 }
