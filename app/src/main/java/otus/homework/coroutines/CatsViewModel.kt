4e39e8065d90703e27bc8185eef8e5dc4e777152
MisterPotz <aleksgornostaev@gmail.com>
2021-10-20 22:48:36 +0300

    - don't break coroutine cancellation mechanism

--- app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
+++ app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
@@ -8,12 +8,17 @@ import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class CatsViewModel(private val catsService: CatsService) : ViewModel() {
-    private val _resultLiveData : MutableLiveData<Result?> = MutableLiveData()
-    val resultLiveData : LiveData<Result?>
+    private val _resultLiveData: MutableLiveData<Result?> = MutableLiveData()
+    val resultLiveData: LiveData<Result?>
         get() = _resultLiveData
-    private val coroutineExceptionHandler = CoroutineExceptionHandler { _: CoroutineContext, _: Throwable ->
-        CrashMonitor.trackWarning()
-    }
+    private val coroutineExceptionHandler =
+        CoroutineExceptionHandler { _: CoroutineContext, throwable: Throwable ->
+            CrashMonitor.trackWarning()
+            throwable.message?.let {
+                _resultLiveData.value = Error(it)
+                _resultLiveData.value = null
+            }
+        }
 
     fun refreshPage() {
         viewModelScope.launch(coroutineExceptionHandler) {
@@ -22,25 +27,13 @@ class CatsViewModel(private val catsService: CatsService) : ViewModel() {
                     val catFact = async { catsService.getCatFact() }
                     val catPicture = async { catsService.getCatPicture() }
 
-                    _resultLiveData.value = Success(CatsViewUiData(catFact.await(), catPicture.await()))
+                    _resultLiveData.value =
+                        Success(CatsViewUiData(catFact.await(), catPicture.await()))
                 }
 
-            } catch (t: Throwable) {
-                when (t) {
-                    is java.net.SocketTimeoutException -> {
-                        _resultLiveData.value = Error("Couldn't fetch response from the server")
-                        _resultLiveData.value = null
-                    }
-                    else -> {
-                        // don't we need to filter if it was coroutine's CancellationException?
-                        // otherwise we could fill up analytics with cancellations as crashes
-                        CrashMonitor.trackWarning()
-                        t.message?.let {
-                            _resultLiveData.value = Error(it)
-                            _resultLiveData.value = null
-                        }
-                    }
-                }
+            } catch (t: java.net.SocketTimeoutException) {
+                _resultLiveData.value = Error("Couldn't fetch response from the server")
+                _resultLiveData.value = null
             }
         }
     }
