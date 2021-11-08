7b98ea2560ef696196613ea6b3ee4f8106a5804d
Alex Kh <alexander@udev.dev>
2021-11-08 09:58:00 +0400

    First part of hw, replace callbacks with coroutins

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,35 +1,55 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.*
+import java.lang.Exception
+import java.net.SocketTimeoutException
+import kotlin.coroutines.CoroutineContext
+
+
+class PresenterScope : CoroutineScope {
+  override val coroutineContext: CoroutineContext =
+    Dispatchers.Main + CoroutineName("CatsCoroutine")
+}
 
 class CatsPresenter(
-    private val catsService: CatsService
+  private val catsService: CatsService
 ) {
 
-    private var _catsView: ICatsView? = null
+  private var _catsView: ICatsView? = null
+  private var job: Job? = null
+  private val scope = PresenterScope()
 
-    fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+  fun onInitComplete() {
+    job = scope.launch {
+      try {
+        val fact = catsService.getCatFact()
+        _catsView?.populate(fact)
+      } catch (e: Exception) {
+        handle(e)
+      }
     }
+  }
 
-    fun attachView(catsView: ICatsView) {
-        _catsView = catsView
+  private fun handle(exception: Exception) = when (exception) {
+    is SocketTimeoutException -> {
+      _catsView?.show("No response from server")
     }
+    else -> {
+      _catsView?.show(exception.message ?: "something nasty happened")
+      CrashMonitor.trackWarning(exception)
+    }
+  }
 
-    fun detachView() {
-        _catsView = null
-    }
+  fun attachView(catsView: ICatsView) {
+    _catsView = catsView
+  }
+
+  fun detachView() {
+    _catsView = null
+  }
+
+  fun onStop() {
+    job?.cancel()
+  }
 }
+
--- app/src/main/java/otus/homework/coroutines/CatsService.kt
+++ app/src/main/java/otus/homework/coroutines/CatsService.kt
@@ -1,10 +1,8 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface CatsService {
-
-    @GET("random?animal_type=cat")
-    fun getCatFact() : Call<Fact>
+  @GET("random?animal_type=cat")
+  suspend fun getCatFact(): Fact
 }
