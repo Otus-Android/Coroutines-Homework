acaaf69ea245cfa551e84df9b4814ff8962a6b61
georgershov <gershov@gmail.com>
2021-11-14 15:07:26 +0300

    fix2

--- app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
+++ app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
@@ -2,31 +2,37 @@ package otus.homework.coroutines
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.*
+import retrofit2.Retrofit
 import java.lang.Exception
 import java.net.SocketTimeoutException
 import kotlin.coroutines.cancellation.CancellationException
 
-class CatsViewModel(private val diContainer: DiContainer): ViewModel(){
-    private val _facts = MutableLiveData<Result>()
+class CatsViewModel(
+    private val serviceFact: FactsService,
+    private val servicePic: PicsService): ViewModel(){
 
-    val handler = CoroutineExceptionHandler { context, e ->
+    private val _facts = MutableLiveData<Result>()
+    private val handler = CoroutineExceptionHandler { context, e ->
         CrashMonitor.trackWarning()
         _facts.value = Error(0, e.message)
     }
+    private val scope = CoroutineScope(Dispatchers.Main)
     val facts: LiveData<Result>
         get() = _facts
     fun loadFacts(){
-        viewModelScope.launch(handler) {
-            val defFact = async(Dispatchers.IO) {
-                diContainer.serviceFact.getCatFact()
-            }
-            val defPic = async(Dispatchers.IO) {
-                diContainer.servicePic.getCatPic()
-            }
-            try {
-                _facts.value = Success(FactAndPicture(defFact.await(), defPic.await()))
-            } catch (e: SocketTimeoutException){
-                _facts.value = Error(msgId = R.string.server_error, msg = null)
+        viewModelScope.launch {
+            scope.launch(handler) {
+                val defFact = async(Dispatchers.IO) {
+                    serviceFact.getCatFact()
+                }
+                val defPic = async(Dispatchers.IO) {
+                    servicePic.getCatPic()
+                }
+                try {
+                    _facts.value = Success(FactAndPicture(defFact.await(), defPic.await()))
+                } catch (e: SocketTimeoutException) {
+                    _facts.value = Error(msgId = R.string.server_error, msg = null)
+                }
             }
         }
     }
@@ -34,7 +40,7 @@ class CatsViewModel(private val diContainer: DiContainer): ViewModel(){
 
 class CatsViewModelFactory(private val di: DiContainer): ViewModelProvider.NewInstanceFactory() {
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>) = CatsViewModel(di) as T
+    override fun <T : ViewModel> create(modelClass: Class<T>) = CatsViewModel(di.serviceFact, di.servicePic) as T
 }
 
 sealed class Result
