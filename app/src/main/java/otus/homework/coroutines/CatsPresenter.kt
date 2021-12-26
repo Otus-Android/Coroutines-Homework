d651a3758f1fd340dc9fb1d2a5c3029cbf9745d5
VladKozyr <vladyslav.kozyr@gmail.com>
2021-12-26 21:51:04 +0200

    task 1: callback to suspend

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,28 +1,32 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.*
+import java.net.SocketTimeoutException
+import kotlin.coroutines.CoroutineContext
 
 class CatsPresenter(
-    private val catsService: CatsService
+    private val catsService: CatsService,
+    private val scope: CoroutineScope
 ) {
 
     private var _catsView: ICatsView? = null
 
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
+        scope.launch {
+            try {
+                _catsView?.populate(catsService.getCatFact())
+            } catch (ex: Exception) {
+                when (ex) {
+                    is SocketTimeoutException -> {
+                        _catsView?.error("Failed to get a response from the server")
+                    }
+                    else -> {
+                        CrashMonitor.trackWarning()
+                        _catsView?.error(ex.message ?: "")
+                    }
                 }
             }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+        }
     }
 
     fun attachView(catsView: ICatsView) {
@@ -31,5 +35,11 @@ class CatsPresenter(
 
     fun detachView() {
         _catsView = null
+        scope.cancel()
     }
+}
+
+class PresenterScope : CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + CoroutineName("CatsCoroutine")
 }
