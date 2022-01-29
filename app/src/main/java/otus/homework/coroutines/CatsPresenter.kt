5772977c50a3919e6c6ed358745be54d703b7371
Marina <quriza@gmail.com>
2022-01-29 16:37:50 +0300

    first

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,35 +1,61 @@
 package otus.homework.coroutines
 
+
+
+import android.widget.Toast
+import kotlinx.coroutines.*
+
+import okhttp3.Dispatcher
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class CatsPresenter(
     private val catsService: CatsService
+
 ) {
 
     private var _catsView: ICatsView? = null
+    private var job: Job? = null
 
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+        getCatsFact()
     }
+
+    fun getCatsFact() {
+        val scope = PresenterScope()
+        job = scope.launch(Dispatchers.Main + CoroutineName("v1coroutine")) {
+            try {
+                coroutineScope {
+                    var one = async {
+
+                        val response = catsService.getCatFact()
+                        _catsView?.populate(response!!)
+                        throw IllegalAccessException("mess")
+
+                    }
+                }
+            } catch (e: java.net.SocketTimeoutException) {
+                _catsView?.showError("Ошибка сокета")
+
+            }
+
+
+        }
+
+    }
+
 
     fun attachView(catsView: ICatsView) {
         _catsView = catsView
     }
 
     fun detachView() {
+        if (job?.isActive == true) {
+            job?.cancel()
+        }
         _catsView = null
     }
+
+
 }
---
+++ app/src/main/java/otus/homework/coroutines/PresenterScope.kt
@@ -0,0 +1,15 @@
+package otus.homework.coroutines
+import android.util.Log
+import kotlinx.coroutines.*
+
+
+private val exceptionHandler =
+    CoroutineExceptionHandler { _, throwable ->
+
+        CrashMonitor.trackWarning()
+    }
+
+class PresenterScope : CoroutineScope {
+    override val coroutineContext = Dispatchers.Main + SupervisorJob() + exceptionHandler
+}
+
