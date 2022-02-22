b6e68dd643393aba23c7bc296649c23cd4a5b459
Sergey <99740680+OkimiAd@users.noreply.github.com>
2022-02-22 10:01:41 +0300

    feat: remake with CoroutineExceptionHandler

--- app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
+++ app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
@@ -4,48 +4,38 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import retrofit2.Response
-import java.lang.Exception
+import kotlin.Exception
 
-class CatsViewModel: ViewModel() {
-      var catsService: CatsService? = null
+class CatsViewModel : ViewModel() {
+    var catsService: CatsService? = null
 
     private var _catsView: ICatsView? = null
 
     private var currentRequest: Job? = null
 
+    private val handler = CoroutineExceptionHandler { _, exception ->
+        if (exception is java.net.SocketTimeoutException)
+            CrashMonitor.trackWarning("Не удалось получить ответ от сервера")
+        else {
+            CrashMonitor.trackWarning("CoroutineExceptionHandler got $exception.message")
+        }
+    }
+
     fun onInitComplete() {
-        currentRequest = viewModelScope.launch {
+        currentRequest = viewModelScope.launch(handler) {
             val fact = async { getFact() }
             val image = async { getRandomImage() }
             _catsView?.populate(FactModel(fact.await()?.text, image.await()?.file))
         }
     }
 
-    // Правильно ли я организовал обработку ошибок?
     private suspend fun getFact(): Fact? {
-        val response = try {
-            withContext(Dispatchers.Default) { catsService?.getCatFact() }
-        } catch (ex: java.net.SocketTimeoutException) {
-            CrashMonitor.trackWarning("Не удалось получить ответ от сервера")
-            return null
-        } catch (ex: Exception) {
-            CrashMonitor.trackWarning(ex.message ?: "Exception")
-            return null
-        }
+        val response = withContext(Dispatchers.Default) { catsService?.getCatFact() }
         return response.getBodyOrNull()
     }
 
     private suspend fun getRandomImage(): Image? {
-        val response = try {
-            withContext(Dispatchers.Default) { catsService?.getRandomImage() }
-        } catch (ex: java.net.SocketTimeoutException) {
-            CrashMonitor.trackWarning("Не удалось получить ответ от сервера")
-            return null
-        } catch (ex: Exception) {
-            CrashMonitor.trackWarning(ex.message ?: "Exception")
-            return null
-        }
-
+        val response = withContext(Dispatchers.Default) { catsService?.getRandomImage() }
         return response.getBodyOrNull()
     }
 
