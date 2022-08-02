7c90ce5311f351b3a416a1a9b545b0c00a781000
PScherbakov <pscher@gmail.com>
2022-08-02 09:40:49 +0300

    Task 3: changes after verification

--- app/src/main/java/otus/homework/coroutines/CatPictureService.kt
+++ app/src/main/java/otus/homework/coroutines/CatPictureService.kt
@@ -1,10 +1,9 @@
 package otus.homework.coroutines
 
-import retrofit2.Response
 import retrofit2.http.GET
 
 interface CatPictureService {
     @GET("meow")
-    suspend fun getCatPicture() : Response<Picture>
+    suspend fun getCatPicture() : Picture
 
 }
