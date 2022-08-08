8e4cd57048a9ee6c22360abbe3a25c019567cf59
Dmitry Sementsov <d.sementsov@evotor.ru>
2022-08-08 12:46:47 +0300

    Move from callbacks to suspend functions and coroutines.

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,28 +1,37 @@
 package otus.homework.coroutines
 
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.coroutines.CoroutineContext
 
+//1. Поменять возвращаемый тип в `CatsService` и добавить модификатор `suspend`
+//2. Переписать логику в презентере с `Callback` на корутины и `suspend` функции
+//3. Реализовать свой скоуп: PresenterScope с `MainDispatcher` и CoroutineName("CatsCoroutine") в качестве элементов контекста
+//4. Добавить обработку исключений через try-catch. В случае `java.net.SocketTimeoutException` показываем Toast с текстом "Не удалось получить ответ от сервером". В остальных случаях логируем исключение в `otus.homework.coroutines.CrashMonitor` и показываем Toast с `exception.message`
+//5. Не забываем отменять Job в `onStop()`
 class CatsPresenter(
     private val catsService: CatsService
 ) {
-
+    private val presenterScope: CoroutineScope =
+        CoroutineScope(Job() + Dispatchers.Main + CoroutineName("CatsCoroutine"))
     private var _catsView: ICatsView? = null
 
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
+        presenterScope.launch {
+            try {
+                val response = catsService.getCatFact()
+                _catsView?.populate(response)
+            } catch (e: Exception) {
+                if (e == java.net.SocketTimeoutException()) {
+                    _catsView?.showToast("Не удалось получить ответ от сервером")
+                } else {
+                    CrashMonitor.trackWarning()
+                    e.message?.let { _catsView?.showToast(it) }
                 }
             }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+        }
     }
 
     fun attachView(catsView: ICatsView) {
@@ -31,5 +40,6 @@ class CatsPresenter(
 
     fun detachView() {
         _catsView = null
+        presenterScope.cancel()
     }
 }
