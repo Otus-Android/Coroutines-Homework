f05a3624e51c11730fa13d0f1f27348565382c9b
Тимур Фаршатов <farshatov@yandex.ru>
2022-09-15 09:47:33 +0500

    исправления по последнему комментарию

--- app/src/main/java/otus/homework/coroutines/MainActivity.kt
+++ app/src/main/java/otus/homework/coroutines/MainActivity.kt
@@ -2,6 +2,7 @@ package otus.homework.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsPresenter = CatsPresenter(diContainer.serviceCats, diContainer.servicePhoto)
+        catsPresenter = CatsPresenter(diContainer.serviceCats, diContainer.servicePhoto, lifecycleScope)
         view.presenter = catsPresenter
         catsPresenter.attachView(view)
         catsPresenter.onInitComplete()
