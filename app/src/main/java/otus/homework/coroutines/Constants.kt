fc32f6d165ed516d01200cf99ad7507403c9b1de
Olga Teplitskaia <zxlnk@yandex.com>
2022-10-12 11:23:29 +0400

    worked server by default

--- app/src/main/java/otus/homework/coroutines/Constants.kt
+++ app/src/main/java/otus/homework/coroutines/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     /** true - work over [CatsPresenter], false - work over [CatsViewModel] */
     const val PRESENTER_VARIANT: Boolean = false
     /** true - take facts from the worked server https://catfact.ninja/fact */
-    const val RESERVE_CATS_SERVER: Boolean = false
+    const val RESERVE_CATS_SERVER: Boolean = true
 }
