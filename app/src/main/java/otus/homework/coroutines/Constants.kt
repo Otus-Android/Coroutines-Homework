e1556ddb643c838ed993b580a8e7e671c489b9ee
Olga Teplitskaia <zxlnk@yandex.com>
2022-10-12 11:18:42 +0400

    Constants - explict types

--- app/src/main/java/otus/homework/coroutines/Constants.kt
+++ app/src/main/java/otus/homework/coroutines/Constants.kt
@@ -2,7 +2,7 @@ package otus.homework.coroutines
 
 object Constants {
     /** true - work over [CatsPresenter], false - work over [CatsViewModel] */
-    const val PRESENTER_VARIANT = false
+    const val PRESENTER_VARIANT: Boolean = false
     /** true - take facts from the worked server https://catfact.ninja/fact */
-    const val RESERVE_CATS_SERVER = false
+    const val RESERVE_CATS_SERVER: Boolean = false
 }
