6775190ac416171dbdad2018a472dc8db0db8927
Maksim Pozdeev <m.l.pozdeev@gmail.com>
2022-10-22 20:59:17 +0400

    Changed callbacks to suspend functions and coroutines

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,8 +1,16 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import android.util.Log
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.net.SocketTimeoutException
+import kotlin.coroutines.CoroutineContext
 
 class CatsPresenter(
     private val catsService: CatsService
@@ -10,19 +18,20 @@ class CatsPresenter(
 
     private var _catsView: ICatsView? = null
 
+    private val presenterScope = PresenterScope()
+
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
+        presenterScope.launch {
+            try {
+                val fact = catsService.getCatFact()
+                _catsView?.populate(fact)
+            } catch (ex: SocketTimeoutException) {
+                _catsView?.showSocketTimeoutExceptionToast()
+            } catch (ex: Exception) {
+                CrashMonitor.trackWarning(CatsPresenter::class.simpleName, ex)
+                _catsView?.showDefaultExceptionToast(ex.message)
             }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+        }
     }
 
     fun attachView(catsView: ICatsView) {
@@ -31,5 +40,14 @@ class CatsPresenter(
 
     fun detachView() {
         _catsView = null
+        presenterScope.cancel()
+    }
+
+    class PresenterScope : CoroutineScope {
+        override val coroutineContext: CoroutineContext = Dispatchers.Main + CoroutineName(COROUTINE_NAME) + Job()
+    }
+
+    companion object {
+        private const val COROUTINE_NAME = "CatsCoroutine"
     }
 }
