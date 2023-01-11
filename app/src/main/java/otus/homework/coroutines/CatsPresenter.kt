2c20e7aa2d671a8775d67361ea3b4214c2d90f47
kamil.tayupov <k-i-tayupov@yandex-team.ru>
2023-01-11 18:07:36 +0300

    Использовал корутины вместо колбеков

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,8 +1,7 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.*
+import java.net.SocketTimeoutException
 
 class CatsPresenter(
     private val catsService: CatsService
@@ -10,19 +9,27 @@ class CatsPresenter(
 
     private var _catsView: ICatsView? = null
 
-    fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
+    private val presenterScope = CoroutineScope(CoroutineName("CatsCoroutine") + Job() + Dispatchers.Main)
+    private var job: Job = Job()
 
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
+    fun onInitComplete() {
+        job = presenterScope.launch {
+            try {
+                val result = withContext(Dispatchers.IO) {
+                    catsService.getCatFact()
+                }
+                _catsView?.populate(result)
+            } catch (e: Exception) {
+                when(e) {
+                    is CancellationException -> throw e
+                    is SocketTimeoutException -> _catsView?.showError("Не удалось получить ответ от сервера")
+                    else -> {
+                        CrashMonitor.trackWarning()
+                        _catsView?.showError(e.message ?: "")
+                    }
                 }
             }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+        }
     }
 
     fun attachView(catsView: ICatsView) {
@@ -31,5 +38,6 @@ class CatsPresenter(
 
     fun detachView() {
         _catsView = null
+        job.cancel()
     }
 }
