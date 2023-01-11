385fbc0b09dadba7df28a9b31616939bd67230bb
Shilov Andrey <andrey.shilov.7991@gmail.com>
2023-01-11 21:30:14 +0300

    #comment switched from callbacks to suspend functions and coroutines

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,8 +1,7 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.*
+import java.net.SocketTimeoutException
 
 class CatsPresenter(
     private val catsService: CatsService
@@ -10,19 +9,34 @@ class CatsPresenter(
 
     private var _catsView: ICatsView? = null
 
+    private val presenterScope = CoroutineScope(
+        Job() + Dispatchers.Main + CoroutineName(COROUTINE_NAME)
+    )
+
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
+        presenterScope.launch {
+            try {
+                doRequest()
+            } catch (e: SocketTimeoutException) {
+                showMessage(SOCKET_TIMEOUT_EXCEPTION_MESSAGE)
+            } catch (e: Exception) {
+                CrashMonitor.trackWarning(e)
+                showMessage(e.message)
             }
+        }
+    }
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
+    private suspend fun doRequest() {
+        val response = catsService.getCatFact()
+        if (response.isSuccessful && response.body() != null) {
+            response.body()?.let { fact ->
+                _catsView?.populate(fact)
             }
-        })
+        }
+    }
+
+    private fun showMessage(message: String?) {
+        _catsView?.showToast(message ?: UNKNOWN_ERROR)
     }
 
     fun attachView(catsView: ICatsView) {
@@ -30,6 +44,13 @@ class CatsPresenter(
     }
 
     fun detachView() {
+        presenterScope.cancel()
         _catsView = null
+    }
+
+    private companion object {
+        private const val COROUTINE_NAME = "CatsCoroutine"
+        private const val UNKNOWN_ERROR = "Неизвестная ошибка"
+        private const val SOCKET_TIMEOUT_EXCEPTION_MESSAGE = "Не удалось получить ответ от сервера"
     }
 }
--- app/src/main/java/otus/homework/coroutines/CrashMonitor.kt
+++ app/src/main/java/otus/homework/coroutines/CrashMonitor.kt
@@ -1,10 +1,14 @@
 package otus.homework.coroutines
 
+import android.util.Log
+
 object CrashMonitor {
 
     /**
      * Pretend this is Crashlytics/AppCenter
      */
-    fun trackWarning() {
+    fun trackWarning(throwable: Throwable) {
+        val errorMessage = throwable.message ?: throwable.localizedMessage ?: ""
+        Log.e("CRASH_MONITOR", errorMessage)
     }
 }
