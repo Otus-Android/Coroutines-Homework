b88f1f1cf165cc8761b215f4c08831256daad444
ivmakar <ivan.r.makarov@yandex.ru>
2023-07-10 09:20:32 +0300

    ADD -- HttpLoggingInterceptor

--- app/src/main/java/otus/homework/coroutines/DiContainer.kt
+++ app/src/main/java/otus/homework/coroutines/DiContainer.kt
@@ -1,12 +1,27 @@
 package otus.homework.coroutines
 
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class DiContainer {
 
     private val retrofit by lazy {
+
+
+        val httpLoggingInterceptor = HttpLoggingInterceptor { s ->
+            Log.i("HttpLog", s)
+        }
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
         Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl("https://catfact.ninja/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
