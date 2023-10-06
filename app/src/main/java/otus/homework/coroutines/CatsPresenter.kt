0f1c0299231c4453e3fe83efb3001286d0bd1735
mvrlrd <mvrlrd@yandex.ru>
2023-10-06 15:43:40 +0300

    use coroutines in presenter

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,31 +1,46 @@
 package otus.homework.coroutines
 
-import android.util.Log
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.net.SocketTimeoutException
 
 class CatsPresenter(
     private val catsService: CatsService
 ) {
-
+    private var job: Job? = null
     private var _catsView: ICatsView? = null
 
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
+        getFactsByCoroutines()
+    }
 
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    Log.e("TAG", "SUCCESS   ${response.body()}")
-                    _catsView?.populate(response.body()!!)
+    private fun getFactsByCoroutines() {
+        job = CoroutineScope(Dispatchers.IO + CoroutineName(CATS_COROUTINE_NAME)).launch {
+            try {
+                val fact = catsService.getCatFactWithCoroutines()
+                withContext(Dispatchers.Main) {
+                    _catsView?.populate(fact)
+                }
+            } catch (ste: SocketTimeoutException) {
+                withContext(Dispatchers.Main) {
+                    _catsView?.showToast(TIME_OUT_EXCEPTION_MESSAGE)
+                }
+            } catch (e: Throwable) {
+                CrashMonitor.trackWarning()
+                withContext(Dispatchers.Main){
+                    e.message?.let{
+                        _catsView?.showToast(it)
+                    }
                 }
             }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                Log.e("TAG", "FAILURE")
-                CrashMonitor.trackWarning()
-            }
-        })
+        }
     }
 
     fun attachView(catsView: ICatsView) {
@@ -34,5 +49,10 @@ class CatsPresenter(
 
     fun detachView() {
         _catsView = null
+        job?.cancel()
+    }
+    companion object{
+        private const val TIME_OUT_EXCEPTION_MESSAGE = "Не удалось получить ответ от сервером"
+        private const val CATS_COROUTINE_NAME = "CatsCoroutine"
     }
 }
