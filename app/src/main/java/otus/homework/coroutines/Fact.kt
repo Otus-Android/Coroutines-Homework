3ea392faf65888da87d78d2d91cb9aec3b2d2a21
mvrlrd <mvrlrd@yandex.ru>
2023-10-06 15:41:44 +0300

    some changes in model

--- app/src/main/java/otus/homework/coroutines/Fact.kt
+++ app/src/main/java/otus/homework/coroutines/Fact.kt
@@ -7,22 +7,22 @@ data class Fact(
 	val fact: String,
 	@field:SerializedName("length")
 	val length: Int,
-	@field:SerializedName("createdAt")
-	val createdAt: String,
-	@field:SerializedName("deleted")
-	val deleted: Boolean,
-	@field:SerializedName("_id")
-	val id: String,
-	@field:SerializedName("text")
-	val text: String,
-	@field:SerializedName("source")
-	val source: String,
-	@field:SerializedName("used")
-	val used: Boolean,
-	@field:SerializedName("type")
-	val type: String,
-	@field:SerializedName("user")
-	val user: String,
-	@field:SerializedName("updatedAt")
-	val updatedAt: String
+//	@field:SerializedName("createdAt")
+//	val createdAt: String,
+//	@field:SerializedName("deleted")
+//	val deleted: Boolean,
+//	@field:SerializedName("_id")
+//	val id: String,
+//	@field:SerializedName("text")
+//	val text: String,
+//	@field:SerializedName("source")
+//	val source: String,
+//	@field:SerializedName("used")
+//	val used: Boolean,
+//	@field:SerializedName("type")
+//	val type: String,
+//	@field:SerializedName("user")
+//	val user: String,
+//	@field:SerializedName("updatedAt")
+//	val updatedAt: String
 )
