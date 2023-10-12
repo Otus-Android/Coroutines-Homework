5581b10bd9f9c71967de2da7958525309fc75480
ZhestovskyYS <aprilgoat17@gmail.com>
2023-10-12 22:52:07 +0600

    Перейти с коллбеков на саспенд функции и корутины

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,35 +1,48 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.SocketTimeoutException
 
 class CatsPresenter(
     private val catsService: CatsService
 ) {
 
     private var _catsView: ICatsView? = null
+    private val presenterScope = CoroutineScope(
+        SupervisorJob() +
+                Dispatchers.Main +
+                CoroutineName("CatsCoroutine")
+    )
 
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
+        presenterScope.launch {
+            try {
+                _catsView?.populate(
+                    catsService.getCatFact()
+                )
+            } catch (_: SocketTimeoutException) {
+                _catsView?.showErrorToast()
+            } catch (e: Exception) {
                 CrashMonitor.trackWarning()
+                _catsView?.showErrorToast(e.message)
             }
-        })
+        }
     }
+
 
     fun attachView(catsView: ICatsView) {
         _catsView = catsView
     }
 
     fun detachView() {
+        presenterScope.coroutineContext.cancelChildren()
         _catsView = null
     }
+
 }
