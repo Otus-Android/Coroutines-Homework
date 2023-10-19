0fafbbb974dd2a3f43e1030ca61775ab35ae8f8f
mvrlrd <mvrlrd@yandex.ru>
2023-10-19 22:58:30 +0300

    refactored Result

--- app/src/main/java/otus/homework/coroutines/presentation/Result.kt
+++ app/src/main/java/otus/homework/coroutines/presentation/Result.kt
@@ -1,6 +1,6 @@
 package otus.homework.coroutines.presentation
 
-sealed class Result<T>(val data: T? = null, val throwable: Throwable? = null) {
-    class Success<T>(data: T): Result<T>(data = data)
-    class Error<T>(throwable: Throwable) : Result<T>(throwable = throwable)
+sealed class Result<T> {
+    class Success<T>(val data: T): Result<T>()
+    class Error<T>(val throwable: Throwable) : Result<T>()
 }
