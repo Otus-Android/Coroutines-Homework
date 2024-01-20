a750c0760052923b9153e4182aed056326d8f50b
shchuchinov_rl <shchuchinov_rl@itms.tech>
2024-01-20 20:26:04 +0300

    feat: HomeWork-02 - Coroutines (tasks 1, 2, 3)

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,35 +1,52 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlin.coroutines.cancellation.CancellationException
 
 class CatsPresenter(
-    private val catsService: CatsService
+    private val catsService: CatsService,
+    private val imagesService: ImagesService,
 ) {
 
+    private var presenterScope: CoroutineScope? = null
     private var _catsView: ICatsView? = null
 
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
+        presenterScope?.launch {
+            try {
+                val fact: Fact
+                val images: List<Image>
+                val factDeferred = async { catsService.getCatFact() }
+                val imagesDeferred = async { imagesService.getImages() }
+                fact = factDeferred.await()
+                images = imagesDeferred.await()
+                _catsView?.populate(CatsUiModel(fact, images.first()))
+            } catch (ce: CancellationException) {
+                throw ce
+            } catch (ste: java.net.SocketTimeoutException) {
+                _catsView?.toast(R.string.http_error_ste)
+            } catch (exception: Exception) {
                 CrashMonitor.trackWarning()
+                _catsView?.toast(R.string.http_error_template, exception.message)
             }
-        })
+        }
     }
 
     fun attachView(catsView: ICatsView) {
         _catsView = catsView
+        presenterScope = CoroutineScope(
+            Dispatchers.Main + CoroutineName("CatsCoroutine") + SupervisorJob()
+        )
     }
 
     fun detachView() {
+        presenterScope?.cancel()
         _catsView = null
     }
 }
--- app/src/main/java/otus/homework/coroutines/DiContainer.kt
+++ app/src/main/java/otus/homework/coroutines/DiContainer.kt
@@ -1,16 +1,40 @@
 package otus.homework.coroutines
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class DiContainer {
+
+    private val httpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
 
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://catfact.ninja/")
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private val imagesRetrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.thecatapi.com/")
+            .client(httpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
     val service by lazy { retrofit.create(CatsService::class.java) }
+
+    val imagesService by lazy { imagesRetrofit.create(ImagesService::class.java) }
+
 }
