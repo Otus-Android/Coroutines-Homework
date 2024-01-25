e48b88c0aad720f166b03bc7fa98679da7309e95
stanislavrepinskas <repinskas@gmail.com>
2024-01-26 00:51:47 +0300

    Added CoroutineExceptionHandler

--- app/src/main/java/otus/homework/coroutines/MainViewModel.kt
+++ app/src/main/java/otus/homework/coroutines/MainViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.net.SocketTimeoutException
@@ -19,37 +21,46 @@ class MainViewModel(
     private val resManager: Resources
 ) : ViewModel() {
 
+    private val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        if (throwable is SocketTimeoutException) {
+            _uiState.value =
+                Result.Error(resManager.getString(R.string.socket_timeout_error))
+        } else {
+            CrashMonitor.trackWarning()
+            _uiState.value = Result.Error(resManager.getString(R.string.unknown_error))
+        }
+    }
+
     private val _uiState = MutableLiveData<Result?>(null)
     val uiState: LiveData<Result?> get() = _uiState
 
     private var job: Job? = null
 
+    private var requestCount = 0
+
     fun updateFact() {
         job?.cancel()
-        job = viewModelScope.launch {
-            try {
-                val result = withContext(Dispatchers.IO) {
-                    val fact = async {
-                        catsService.getCatFact()
-                    }
-                    val image = async {
-                        imageService.getCatImages().firstOrNull()
-                    }
-                    FactState(
-                        fact = fact.await(),
-                        image = image.await()
-                    )
+        job = viewModelScope.launch(exceptionHandler) {
+            requestCount++
+            val result = withContext(Dispatchers.IO) {
+                val fact = async {
+                    catsService.getCatFact()
                 }
-                _uiState.value = Result.Success(result)
-            } catch (e: Exception) {
-                if (e is SocketTimeoutException) {
-                    _uiState.value =
-                        Result.Error(resManager.getString(R.string.socket_timeout_error))
-                } else {
-                    CrashMonitor.trackWarning()
-                    _uiState.value = Result.Error(resManager.getString(R.string.unknown_error))
+                val image = async {
+                    imageService.getCatImages().firstOrNull()
                 }
+
+                // Для теста exceptionHandler
+                if(requestCount % 2 == 0) {
+                    throw SocketTimeoutException()
+                }
+
+                FactState(
+                    fact = fact.await(),
+                    image = image.await()
+                )
             }
+            _uiState.value = Result.Success(result)
         }
     }
 
