ffeb5b20ebb9633d60fbcccb32b5749a51e2ffe5
Danila <danila.filin@yandex.ru>
2024-02-02 12:12:52 +0300

    ### Перейти с коллбеков на саспенд функции и корутины

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,8 +1,10 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class CatsPresenter(
     private val catsService: CatsService
@@ -10,19 +12,26 @@ class CatsPresenter(
 
     private var _catsView: ICatsView? = null
 
+    private val presenterScope =
+        CoroutineScope(CoroutineName("CatsCoroutine") + Dispatchers.Main.immediate)
+
+    private var job: Job? = null
+
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
 
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
-            }
+        job = presenterScope.launch {
+            try {
+                val fact = catsService.getCatFact()
+                _catsView?.populate(fact)
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
+            } catch (e1: java.net.SocketTimeoutException) {
+                _catsView?.showToast("Не удалось получить ответ от сервера")
+            } catch (e: Exception) {
+                _catsView?.showToast(e.toString())
                 CrashMonitor.trackWarning()
             }
-        })
+        }
+
     }
 
     fun attachView(catsView: ICatsView) {
@@ -30,6 +39,7 @@ class CatsPresenter(
     }
 
     fun detachView() {
+        job?.cancel()
         _catsView = null
     }
 }
